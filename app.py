982e8e7f9b4226a2a3f6de15acc170f410386267
Davi Câmara <72800828+DaviCamara@users.noreply.github.com>
2023-11-06 10:31:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,38 +8,37 @@ from io import BytesIO
 
 app = Flask(__name__)
 
-# @app.route('/', methods=['GET'])
-# def hello_geek():
-#     return '<h1>Hello from Flask & Docker</h2>'
+@app.route('/', methods=['GET'])
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
 @app.route('/', methods=['POST'])
 def upload_audio():
     print("entrei")
-    # if 'audio' not in request.files:
-    #     return "No file part", 400
+    if 'audio' not in request.files:
+        return "No file part", 400
 
-    # audio_file = request.files['audio'] 
+    audio_file = request.files['audio'] 
 
-    # if audio_file.filename == '':
-    #     return "No selected file", 400
+    if audio_file.filename == '':
+        return "No selected file", 400
 
-    # if audio_file:
+    if audio_file:
         
-    #     ogg_path = 'uploaded_audio.ogg'
-    #     audio_file.save(ogg_path)
+        ogg_path = 'uploaded_audio.ogg'
+        audio_file.save(ogg_path)
 
-    #     # Converter o arquivo OGG para WAV
-    #     audio_data = AudioSegment.from_file(ogg_path, format="ogg")
-    #     wav_path = 'aaaaaaa.wav'
-    #     audio_data.export(wav_path, format="wav")
-
-        
-    #     # Agora você pode passar o arquivo WAV para a função criar_dataframe()
-    #     avaliar_modelo(wav_path)
+        # Converter o arquivo OGG para WAV
+        audio_data = AudioSegment.from_file(ogg_path, format="ogg")
+        wav_path = 'aaaaaaa.wav'
+        audio_data.export(wav_path, format="wav")
 
         
-    #     return avaliar_modelo(wav_path)
-return "hello"
+        # Agora você pode passar o arquivo WAV para a função criar_dataframe()
+        avaliar_modelo(wav_path)
+
+        
+        return avaliar_modelo(wav_path)
     # #"Audio file received and converted to WAV successfully", 200
         
 
